9153f02d3850d713d000e31cdbe7cca1dac1fb29
RedSkyi <24255551+RedSkyi@users.noreply.github.com>
2020-05-14 22:32:41 +0200

    Fix hitbox drawing & add draw method

--- src/sprite.py
+++ src/sprite.py
@@ -15,13 +15,15 @@ class Sprite:
         return self.x <= sprite.x <= self.x + self.width and self.y <= sprite.y + sprite.height <= self.y + self.height
 
     def draw_hitbox(self):
-        EZ.trace_segment(self.x, self.y, self.x + self.height, self.y)
-        EZ.trace_segment(self.x, self.y, self.x, self.y + self.width)
-        EZ.trace_segment(self.x + self.height, self.y, self.x + self.height, self.y + self.width)
-        EZ.trace_segment(self.x, self.y + self.width, self.x + self.height, self.y + self.width)
+        EZ.trace_segment(self.x, self.y, self.x + self.width, self.y)
+        EZ.trace_segment(self.x, self.y, self.x, self.y + self.height)
+        EZ.trace_segment(self.x + self.width, self.y, self.x + self.width, self.y + self.height)
+        EZ.trace_segment(self.x, self.y + self.height, self.x + self.width, self.y + self.height)
 
     def update(self):
-        # TODO: Draw sprites
+        pass
+
+    def draw(self):
         pass
 
 
